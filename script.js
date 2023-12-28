801666a801b1f584a523b45400df0535245ee4a2
FatihChdr <116728558+FatihChdr@users.noreply.github.com>
2023-12-28 20:08:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HTML'den gelen elementleri seçme
 const word_el = document.getElementById('word');
 const popup = document.getElementById('popup-container');
 const message_el = document.getElementById('success-message');
@@ -9,20 +10,51 @@ const easyBtn = document.getElementById('easy-btn');
 const mediumBtn = document.getElementById('medium-btn');
 const hardBtn = document.getElementById('hard-btn');
 const scoreValue = document.getElementById('score-value');
-const letterInput = document.getElementById('letter-input');
+const keyboardContainer = document.getElementById('keyboard-container');
+const keyboardButtons = document.querySelectorAll('.keyboard-key');
 
+// Oyun için gerekli değişkenler
 let correctLetters = [];
 let wrongLetters = [];
 let selectedWord;
 let difficultyLevel = 'easy'; // Varsayılan zorluk seviyesi
 let score = 0;
 
+// Ekran klavyesi düğmelerine tıklama olayını ekle
+keyboardButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const clickedLetter = button.innerText;
+        checkLetter(clickedLetter);
+    });
+});
+
+// Harfi kontrol etme fonksiyonu
+function checkLetter(letter) {
+    letter = letter.toUpperCase(); // Harfi büyük harfe çevir
+
+    if (correctLetters.includes(letter) || wrongLetters.includes(letter)) {
+        // Bu harfi zaten kontrol ettik, mesajı göster
+        displayMessage("Bu harfi zaten girdiniz.");
+    } else {
+        // Harfi kontrol et ve duruma göre işlem yap
+        if (selectedWord.includes(letter)) {
+            correctLetters.push(letter);
+            displayWord();
+        } else {
+            wrongLetters.push(letter);
+            updateWrongLetters();
+        }
+    }
+}
+
+// Kelime listesi
 const wordLists = {
     easy: ['gül', 'kelebek', 'kuş', 'deniz', 'orman'],
     medium: ['papatya', 'şelale', 'dağ', 'gökkuşağı', 'balık'],
     hard: ['volkan', 'şimşek', 'gizemli', 'geceyarısı', 'cadılarbayramı'],
 };
 
+// Tema renkleri
 const themes = {
     easy: {
         backgroundColor: '#87CEEB',  // Light Blue
@@ -41,11 +73,39 @@ const themes = {
     },
 };
 
+// Tema uygulama fonksiyonu
 function applyTheme(theme) {
     document.body.style.backgroundColor = theme.backgroundColor;
     document.body.style.color = theme.textColor;
+    easyBtn.style.backgroundColor = theme.buttonColor;
+    mediumBtn.style.backgroundColor = theme.buttonColor;
+    hardBtn.style.backgroundColor = theme.buttonColor;
+    // Diğer elementlerin temalarını da aynı şekilde ayarlayabilirsiniz
 }
 
+// Temayı uygula
+applyTheme(themes.easy);
+
+// Oyunu başlatma fonksiyonu
+function startGame() {
+    correctLetters = [];
+    wrongLetters = [];
+    selectedWord = getRandomWord();
+
+    displayWord();
+    updateWrongLetters();
+
+    popup.style.display = 'none';
+}
+
+// Zorluk seviyesi değiştirme fonksiyonu
+function changeDifficulty(level) {
+    difficultyLevel = level;
+    startGame();
+    applyTheme(themes[level]);
+}
+
+// Kelime listesinden rastgele bir kelime seçme fonksiyonu
 function getRandomWord() {
     const words = wordLists[difficultyLevel];
     let newWord;
@@ -54,9 +114,10 @@ function getRandomWord() {
         newWord = words[Math.floor(Math.random() * words.length)];
     } while (newWord === selectedWord);
 
-    return newWord;
+    return newWord.toUpperCase(); // Seçilen kelimeyi büyük harfe çevirerek döndür
 }
 
+// Kelimeyi ekranda gösterme fonksiyonu
 function displayWord() {
     word_el.innerHTML = selectedWord
         .split('')
@@ -75,6 +136,7 @@ function displayWord() {
     }
 }
 
+// Yanlış harfleri güncelleme fonksiyonu
 function updateWrongLetters() {
     wrongLetters_el.innerHTML = `
         ${wrongLetters.length > 0 ? '<h3>Hatalı Harfler</h3>' : ''}
@@ -93,11 +155,13 @@ function updateWrongLetters() {
     if (wrongLetters.length === items.length) {
         popup.style.display = 'flex';
         message_el.innerText = `Malesef kaybettiniz. Doğru kelime: ${selectedWord}`;
-        updateScore(-score);
+        updateScore(-score); // Skoru sıfırla
     }
 }
 
-function displayMessage() {
+// Mesajı ekranda gösterme fonksiyonu
+function displayMessage(msg) {
+    message_el.innerText = msg;
     message.classList.add('show');
 
     setTimeout(function () {
@@ -105,25 +169,21 @@ function displayMessage() {
     }, 2000);
 }
 
+// Skoru güncelleme fonksiyonu
 function updateScore(value) {
     score += value;
     if (value < 0) {
-        score = 0;
+        score = 0; // Eğer yanlış bilirse skoru sıfırla
     }
     scoreValue.innerText = score;
 }
 
+// Yeniden oyunu başlatma butonuna tıklama olayını dinle
 playAgainBtn.addEventListener('click', function () {
-    correctLetters = [];
-    wrongLetters = [];
-    selectedWord = getRandomWord();
-
-    displayWord();
-    updateWrongLetters();
-
-    popup.style.display = 'none';
+    startGame();
 });
 
+// Zorluk seviyelerine tıklama olayını dinle
 easyBtn.addEventListener('click', function () {
     changeDifficulty('easy');
 });
@@ -136,46 +196,5 @@ hardBtn.addEventListener('click', function () {
     changeDifficulty('hard');
 });
 
-function restartGame() {
-    correctLetters = [];
-    wrongLetters = [];
-    selectedWord = getRandomWord();
-
-    displayWord();
-    updateWrongLetters();
-
-    popup.style.display = 'none';
-}
-
-function changeDifficulty(level) {
-    difficultyLevel = level;
-    restartGame();
-    applyTheme(themes[difficultyLevel]);
-}
-
-letterInput.addEventListener('input', function (e) {
-    const letter = e.target.value.toLowerCase();
-
-    if (/^[a-zçğıöşü]$/.test(letter)) {
-        if (selectedWord.includes(letter)) {
-            if (!correctLetters.includes(letter)) {
-                correctLetters.push(letter);
-                displayWord();
-            } else {
-                displayMessage();
-            }
-        } else {
-            if (!wrongLetters.includes(letter)) {
-                wrongLetters.push(letter);
-                updateWrongLetters();
-            } else {
-                displayMessage();
-            }
-        }
-    }
-
-    e.target.value = '';
-});
-
-applyTheme(themes.easy);
-displayWord();
+// Oyunu başlat
+startGame();
